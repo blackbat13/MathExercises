8b3c4e2fccc35c970900db145fc716ab197b50e5
blackbat13 <blackbat13@gmail.com>
2023-09-11 18:35:38 +0200

    Update different_numbers.py

--- combinatorics/numbers/different_numbers.py
+++ combinatorics/numbers/different_numbers.py
@@ -1,12 +1,14 @@
 from itertools import permutations
 
+
 def different_numbers(digits: list) -> list:
     """
     Generates all numbers that can be created using the given digits.
     """
-    numbers = [int("".join(map(str,el))) for el in permutations(digits)]
+    numbers = [int("".join(map(str, el))) for el in permutations(digits)]
     numbers = list(filter(lambda x: len(str(x)) == len(digits), numbers))
     return numbers
+
 
 def run_diffent_numbers(digits: list, print_numbers: bool = False):
     result = different_numbers(digits)
@@ -19,10 +21,10 @@ def run_diffent_numbers(digits: list, print_numbers: bool = False):
 
 
 if __name__ == "__main__":
-    run_diffent_numbers([5, 6, 7, 8, 9], True)
+    run_diffent_numbers([5, 6, 7, 8, 9], True)  # 120
     print()
-    run_diffent_numbers([2, 4, 5, 7, 6, 8])
+    run_diffent_numbers([2, 4, 5, 7, 6, 8])  # 720
     print()
-    run_diffent_numbers([i for i in range(1, 10)])
+    run_diffent_numbers([i for i in range(1, 10)])  # 362880
     print()
-    run_diffent_numbers([i for i in range(0, 10)])
+    run_diffent_numbers([i for i in range(0, 10)])  # 3265920
